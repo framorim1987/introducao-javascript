e6009ccc2865d1ecae2554de09009b440175de3c
framorim1987 <framorim1987@gmail.com>
2020-06-27 18:40:44 -0300

    arrays, loops e estilos

--- js/principal.js
+++ js/principal.js
@@ -1,34 +1,46 @@
+// alterando o título
 var titulo = document.querySelector('.titulo');
 titulo.textContent = 'Aparecida Nutricionista';
 
-// obtendo os dados da tabela
-var paciente = document.querySelector('#primeiro-paciente');
+// criando lista dos pacientes
+var pacientes = document.querySelectorAll('.paciente');
 
-var tdPeso = paciente.querySelector('.info-peso');
-var peso = tdPeso.textContent;
+//percorrendo a lista de pacientes
+for (var i = 0; i < pacientes.length; i++) {
 
-var tdAltura = paciente.querySelector('.info-altura');
-var altura = tdAltura.textContent;
+    //obtendo os dados dos pacientes
+    var paciente = pacientes[i];
 
-// validando os valores obtdos
+    var tdPeso = paciente.querySelector('.info-peso');
+    var peso = tdPeso.textContent;
 
-var pesoValido = true;
-var alturaValida = true;
+    var tdAltura = paciente.querySelector('.info-altura');
+    var altura = tdAltura.textContent;
 
-if (peso <= 0 || peso >= 1000) {
-    tdImc.textContent = 'Peso Inválido';
-    pesoValido = false;
-}
-
-if (altura <= 0 || altura >= 3) {
-    tdImc.textContent = 'Altura Inválida';
-    alturaValida = false;
-}
-
-// calculando o IMC
-if (pesoValido && alturaValida) {
-    var imc = peso / (altura * altura);
-    // aplicando o valor na tabela
     var tdImc = paciente.querySelector('.info-imc');
-    tdImc.textContent = imc;
+
+    // validando os valores obtdos
+    var pesoValido = true;
+    var alturaValida = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        tdImc.textContent = 'Peso Inválido';
+        pesoValido = false;
+        // paciente.style.backgroundColor = 'lightcoral';
+        paciente.classList.add('paciente-invalido');
+    }
+
+    if (altura <= 0 || altura >= 3) {
+        tdImc.textContent = 'Altura Inválida';
+        alturaValida = false;
+        // paciente.style.backgroundColor = 'lightcoral';
+        paciente.classList.add('paciente-invalido');
+    }
+
+    // calculando o IMC
+    if (pesoValido && alturaValida) {
+        var imc = peso / (altura * altura);
+        // aplicando o valor na tabela
+        tdImc.textContent = imc.toFixed(2);
+    }
 }
